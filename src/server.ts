2dd3c15cdec6156c9ec6c780cf38fa0bc562cdab
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-07-08 23:47:43 +0600

    app.ts, server.ts & mongodb connection completed

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import { Server } from "http";
+import app from "./app";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const PORT = 3000;
+let server: Server;
+
+
+async function main(){
+    try {
+        await mongoose.connect('mongodb+srv://librarymanagementapi:librarymanagementapi@librarymanagementapi.lf7sw63.mongodb.net/?retryWrites=true&w=majority&appName=librarymanagementapi');
+        console.log("Connected to MongoDB")
+        app.listen(PORT, ()=> {
+            console.log(`Library management API is running on : ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
